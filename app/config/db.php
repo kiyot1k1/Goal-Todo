95cfb4ccadaf116476b26ca3b8bdda18521f1859
kiyot1k1 <kt.n.sfbe@gmail.com>
2021-12-09 20:12:18 +0900

    update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,9 +1,9 @@
 <?php
 
-// const DSN = 'mysql:dbname=mydb; host=mysql; charset=utf8';
-// const USERNAME = 'kiyo';
-// const PASSWORD = 'password';
+const DSN = 'mysql:dbname=mydb; host=mysql; charset=utf8';
+const USERNAME = 'kiyo';
+const PASSWORD = 'password';
 
-const DSN = 'mysql:dbname=heroku_68938efde0e573f; host=us-cdbr-east-04.cleardb.com; charset=utf8';
-const USERNAME = 'bca4e6b9d4fe29';
-const PASSWORD = 'c34b77fe';
+// const DSN = 'mysql:dbname=heroku_68938efde0e573f; host=us-cdbr-east-04.cleardb.com; charset=utf8';
+// const USERNAME = 'bca4e6b9d4fe29';
+// const PASSWORD = 'c34b77fe';
