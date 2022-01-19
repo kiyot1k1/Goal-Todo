8cfe113620644c775749f543373c0ccc2ee36864
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-01-19 11:23:22 +0900

    update TodoController.php

--- app/controller/TodoController.php
+++ app/controller/TodoController.php
@@ -40,8 +40,7 @@ class GoalController {
       return;
     }
 
-    // header("Location: __DIR__ . '/../index.php'");
-    header("Location: https://goal-todo.herokuapp.com/index.php");
+    header("Location: ./../index.php");
   }
 
   public function goal_edit() {
