d878c0ec232946fd2e430be649000d4e3259c158
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-02-17 14:38:47 +0900

    update index.php

--- app/index.php
+++ app/index.php
@@ -42,15 +42,6 @@ if (isset($_POST['todo_id'])) {
         </div>
       </div>
       <h1>目標とTODOの管理ツール</h1>
-      <!-- <div class="header-zone">
-        <div>
-          <img src="./img/Goal.jpg" alt="目標の写真" class="goal-pic">
-        </div>
-        <h1>目標 ＆ TODOリスト</h1>
-        <div>
-          <img src="./img/Todolist.jpg" alt="TODOの写真" class="todo-pic">
-        </div>
-      </div> -->
     </a>
   </header>
 
