0e281189c328ebd52c1b44011f73e19dba13ad22
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-02-20 18:08:16 +0900

    update join.php

---
+++ app/user/join.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>目標設定</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="./../public/css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <header>
+    <a href="./../index.php">
+      <div class="header-zone">
+        <div>
+          <img src="./../img/Goal.jpg" alt="目標の写真" class="goal-pic">
+        </div>
+        <div>
+          <img src="./../img/Todolist.jpg" alt="TODOの写真" class="todo-pic">
+        </div>
+      </div>
+      <h1>目標とTODOの管理ツール</h1>
+    </a>
+  </header>
