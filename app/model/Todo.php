885cc982ce5f39f307326d5ee54cd8fdbc390b19
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-02-25 19:09:26 +0900

    update Todo.php

--- app/model/Todo.php
+++ app/model/Todo.php
@@ -341,25 +341,25 @@ class User {
     $this->name = $name;
   }
 
-  public function getName() {
-    return $this->name;
-  }
+  // public function getName() {
+  //   return $this->name;
+  // }
   
   public function setMail($mail) {
     $this->mail = $mail;
   }
 
-  public function getMail() {
-    return $this->mail;
-  }
+  // public function getMail() {
+  //   return $this->mail;
+  // }
 
   public function setPass($pass) {
     $this->pass = $pass;
   }
 
-  public function getPass() {
-    return $this->pass;
-  }
+  // public function getPass() {
+  //   return $this->pass;
+  // }
 
   public static function join() {
     try {
