a5aa4b796d7096738ba926f60645c6841645985c
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-03-22 17:33:42 +0900

    file update

--- app/view/todo/new.php
+++ app/view/todo/new.php
@@ -48,7 +48,7 @@ if (isset($_SESSION['error_msgs'])) {
           <img src="./../../img/Todolist.jpg" alt="TODOの写真" class="todo-pic">
         </div>
       </div>
-      <h1>目標とTODOの管理ツール</h1>
+      <h1>目標とTODOの管理アプリ</h1>
     </a>
   </header>
   
