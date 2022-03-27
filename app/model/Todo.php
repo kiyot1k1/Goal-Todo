e7ee21f4dbb316859b60a7e094c9c7b5a0156c8f
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-03-27 20:16:46 +0900

    file update

--- app/model/Todo.php
+++ app/model/Todo.php
@@ -358,6 +358,8 @@ class User {
 
     $query =  "SELECT COUNT(*) AS cnt FROM user WHERE mail = ?";
     $stmh = $pdo->query($query);
+    var_dump($stmh);
+    exit;
     $stmh->execute();
     $record = $stmh->fetchAll(PDO::FETCH_ASSOC);
     
