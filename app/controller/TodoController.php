badfede4b14be0e001f96e9a2532887868228f72
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-04-30 17:52:58 +0900

    file update

--- app/controller/TodoController.php
+++ app/controller/TodoController.php
@@ -390,7 +390,7 @@ class UserController {
       $_SESSION['id'] = $result['id'];
       $_SESSION['name'] = $result['name'];
       $_SESSION['time'] = time();
-      header("Location: https://goal-todo.herokuapp.com/index.php");
+      header("Location: ./../index.php");
       return;
     }
   }
