dfeaf416bc5c67a1e8373ae3b8193e040f15b114
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-05-02 20:29:15 +0900

    file update

--- app/index.php
+++ app/index.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['name'])) {
 
 if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
   $_SESSION['time'] = time();
-  $msg = 'ようこそ' . htmlspecialchars($username, ENT_QUOTES) . 'さん。<br>TODOを管理して、目標を達成しましょう！';
+  $msg = htmlspecialchars($username, ENT_QUOTES) . 'さん。<br>TODOを管理して、目標を達成しましょう！';
   $logout = '<a href="./user/logout.php" class="top-logout">ログアウト</a>';
 } else {
   $msg = '※ログインして下さい';
