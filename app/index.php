da00b1fe1d1978c154d6cb3e2c8eb6e961f470fe
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-05-09 17:49:52 +0900

    file update

--- app/index.php
+++ app/index.php
@@ -63,7 +63,7 @@ if (isset($_POST['todo_id'])) {
       <?php echo $msg; ?>
     </div>  
 
-    <div class="user">
+    <div class="top-logout-area">
       <!-- <a href="./user/join.php" class="user-registration">ユーザー登録</a>
       <a href="./user/login.php" class="login">ログイン</a> -->
       <!-- <?php if ($logout): ?> -->
