26487c775409ca2796795125b8363f4343b89633
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-07-11 19:48:56 +0900

    file update

--- app/model/Todo.php
+++ app/model/Todo.php
@@ -35,7 +35,7 @@ class Goal {
     };
 
     // $stmh = $pdo->query("SELECT * FROM goals g, users u WHERE g.user_id = u.id");
-    $stmh = $pdo->query("SELECT g.id, g.goal, g.completed_at, g.created_at, g.updated_at, g.deleted_at, g.user_id FROM goals g, users u WHERE g.user_id = u.id");
+    $stmh = $pdo->query("SELECT g.id, goal, g.completed_at, g.created_at, g.updated_at, g.deleted_at, user_id FROM goals g, users u WHERE g.user_id = u.id");
     // $stmh = $pdo->query("SELECT * FROM goals");
     $goal_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
 
