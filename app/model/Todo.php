2e5b1ed541bb8412779b96d1d7d01c49d7ceed0a
kiyot1k1 <kt.n.sfbe@gmail.com>
2022-07-11 10:20:01 +0900

    file update

--- app/model/Todo.php
+++ app/model/Todo.php
@@ -34,7 +34,7 @@ class Goal {
       echo 'DB接続エラー： ' . $e->getMessage();
     };
 
-    $stmh = $pdo->query("SELECT g.id, g.goal, g.completed_at, g.created_at, g.updated_at, g.deleted_at, g.user_id FROM goals g, users u WHERE g.user_id = users.id");
+    $stmh = $pdo->query("SELECT goals.id, goals.goal, goals.completed_at, goals.created_at, goals.updated_at, goals.deleted_at, goals.user_id FROM goals g, users u WHERE g.user_id = users.id");
     // $stmh = $pdo->query("SELECT * FROM goals");
     $goal_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
 
